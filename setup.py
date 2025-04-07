697c1cb134f223fbaa1085f2119b8e495115cb0f
TechWizDhruv <dhruvgandhi250@gmail.com>
2025-04-07 19:19:23 +0530

    folder structure created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medibot Generative AI Project',
+    version= '0.0.0',
+    author= 'Dhruv Gandhi ',
+    author_email= 'dhruvgandhi250@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
